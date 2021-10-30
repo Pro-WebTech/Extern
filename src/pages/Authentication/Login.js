d59a8932f785657c2545762d7da43007a99ed07c
Super-Smile <baydev1998@gmail.com>
2021-10-30 04:30:40 +0300

    sec change text

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -147,7 +147,7 @@ class Login extends Component {
                                             <Col lg={7}>
                                                 <div>
                                                     <div className="text-center">
-                                                        <h4 className="login-title-font">Login</h4>
+                                                        <h4 className="login-title-font">LLOOGGIINN</h4>
                                                         <p className="login-title-content">Para começar nossa jornada, selecione o tipo de plano que você gostaria de experimentar</p>
                                                     </div>
 
@@ -183,7 +183,7 @@ class Login extends Component {
                                                                 <Col className="line-center">
                                                                 </Col>
                                                                 <Col style={{ fontSize: "16px" }}>
-                                                                    ou efetue o login com
+                                                                    ou efetue o login   com
                                                                 </Col>
                                                                 <Col className="line-center">
                                                                 </Col>
